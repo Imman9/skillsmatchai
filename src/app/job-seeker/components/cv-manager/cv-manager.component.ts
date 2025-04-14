ba0c8865fb4b8b5e18eb71b067b38830de5f0d3c
imman9 <kipkoechemmanuel234@gmail.com>
2025-04-14 13:06:12 +0300

    Ui

--- src/app/job-seeker/components/cv-manager/cv-manager.component.ts
+++ src/app/job-seeker/components/cv-manager/cv-manager.component.ts
@@ -119,7 +119,7 @@ interface CV {
 
               <p>{{ cv.summary }}</p>
               <div class="cv-details">
-                <span>Phone: {{ cv.phone }}</span>
+                <span>Phone No: {{ cv.phone }}</span>
                 <a
                   *ngIf="cv.link"
                   [href]="cv.link"
